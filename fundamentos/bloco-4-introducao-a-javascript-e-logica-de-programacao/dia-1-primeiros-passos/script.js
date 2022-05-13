ea0ff0f1bd0c221ae5a2f64ef22cf8b469a4e6b4
mjggel <luismiguelvieira015@gmail.com>
2022-05-13 20:24:29 -0300

    Requisito 10 exercicios 4.1

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-primeiros-passos/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-primeiros-passos/script.js
@@ -148,7 +148,7 @@ if((a||b||c)% 2 == 0){
 }*/
 // Escreva um programa que defina três números em constantes e retorne true se pelo menos uma das três for ímpar. Caso contrário, ele retorna false.
 // Bonus: use somente um if.
-const a = 1;
+/*const a = 1;
 const b = 2;
 const c = 3;
 
@@ -156,4 +156,24 @@ if((a||b||c)%2!=0){
     console.log("true")
 }else{
     console.log("false")
+}*/
+
+// Escreva um programa que se inicie com dois valores em duas constantes diferentes: o custo de um produto e seu valor de venda. A partir dos valores, calcule quanto de lucro (valor de venda descontado o custo do produto) a empresa terá ao vender mil desses produtos.
+// Atente que, sobre o custo do produto, incide um imposto de 20%.
+// Seu programa também deve emitir uma mensagem de erro e encerrar caso algum dos seus valores de entrada seja menor que zero.
+// O lucro de um produto é o resultado da subtração do valor de venda pelo custo do mesmo, sendo que o imposto de 20% também faz parte do valor de custo.
+// valorCustoTotal = valorCusto + impostoSobreOCusto
+// lucro = valorVenda - valorCustoTotal (lucro de um produto)
+
+const valorCusto = -150;
+const valorVenda = -150;
+
+let impostoSobreOCusto = (valorCusto/100)*20;
+let valorCustoTotal = valorCusto+impostoSobreOCusto;
+let lucro = valorVenda - valorCustoTotal;
+
+if(valorCusto<0||valorVenda<0){
+    console.log("ERRO!!!!")
+}else{
+    console.log("O lucro após mil vendas séra: ", lucro *1000);
 }
