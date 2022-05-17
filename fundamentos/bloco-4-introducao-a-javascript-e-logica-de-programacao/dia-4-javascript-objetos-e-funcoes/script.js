e750b806d78b24cb26f6733c3fc30b13eb98b895
mjggel <luismiguelvieira015@gmail.com>
2022-05-17 18:21:22 -0300

    REQUISITO 2 e 3 EXERCICIO 4.4

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/script.js
@@ -1,3 +1,5 @@
+// 1 - Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome.
+
 let info = {
     personagem: 'Margarida',
     origem: 'Pato Donald',
@@ -5,4 +7,16 @@ let info = {
   };
 
 
-    console.log("Boas Vindas ", info.personagem);
+console.log("Boas Vindas ", info.personagem);
+
+// 🚀 2 - Insira no objeto uma nova propriedade com o nome de chave 'recorrente' e o valor 'Sim' e, em seguida, imprima o objeto no console.
+
+info.recorrente = 'Sim',
+
+console.log(info)
+
+// 3 - Faça um for/in que mostre todas as chaves do objeto.
+
+for(let key in info){
+    console.log(key)
+}
