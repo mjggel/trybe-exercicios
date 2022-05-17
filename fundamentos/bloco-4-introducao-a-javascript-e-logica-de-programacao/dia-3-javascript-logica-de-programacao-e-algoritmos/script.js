eaa1a7d6cc369ff9e8454da21d03507c3323465e
mjggel <luismiguelvieira015@gmail.com>
2022-05-17 20:42:28 -0300

    exercicios por fazer

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
@@ -1,6 +1,17 @@
-let fatorial = 10;
-let resultado = fatorial;
+// let fatorial = 10;
+// let resultado = fatorial;
 
-for(let i = 1; i < fatorial; i++){
-  resultado *= i;
-}console.log(resultado);
+// for(let i = 1; i < fatorial; i++){
+//   resultado *= i;
+// }console.log(resultado);
+
+let word = 'tryber';
+
+// let wordReverse = [];
+// for(let i = 0; i < word.length; i += 1){
+//     wordReverse += i;
+//     if (wordReverse == i){
+//         console.log()
+//     }
+// }
+console.log(word.length)
