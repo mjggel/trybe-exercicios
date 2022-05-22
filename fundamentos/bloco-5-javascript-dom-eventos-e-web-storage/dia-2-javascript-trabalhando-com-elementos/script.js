73097a42410504bca4d7dad4a4f43df97c024748
mjggel <luismiguelvieira015@gmail.com>
2022-05-21 21:39:30 -0300

    exerciicos 6 bloco 5 dia 2

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
@@ -24,6 +24,10 @@ let section1 = document.createElement('section');
 main.appendChild(section1);
 
 // 🚀 Adicione a tag section com a classe right-content como filho da tag main criada no passo 2;
+let section2 = document.createElement('section');
+    section2.className = 'rigth-content';
+main.appendChild(section2);
+
 // 🚀 Adicione uma imagem com src configurado para o valor https://picsum.photos/200 e classe small-image. Esse elemento deve ser filho do section criado no passo 5;
 // 🚀 Adicione uma lista não ordenada com os valores de 1 a 10 por extenso, ou seja, um, dois, três, ... como valores da lista. Essa lista deve ser filha do section criado no passo 6;
 // 🚀 Adicione 3 tags h3, todas sendo filhas do main criado no passo 2.
