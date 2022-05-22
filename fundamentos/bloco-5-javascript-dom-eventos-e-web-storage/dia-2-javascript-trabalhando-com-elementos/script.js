fd2ad4b13113e8e2ad5942e1a02b4441312db3b1
mjggel <luismiguelvieira015@gmail.com>
2022-05-21 22:15:10 -0300

    exercicios 4 da segunda parte bloco 5 dia 2

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
@@ -63,6 +63,8 @@ for (let sla = 0; index <= 2; index += 1){
 //3. 🚀 Remova a section criado no passo 5 (aquele que possui a classe left-content). Utilize a função .removeChild();
 main.removeChild(section2);
 
-// 🚀 Centralize a section criado no passo 6 (aquele que possui a classe right-content). Dica: para centralizar, basta configurar o margin-right: auto da section;
+//4. 🚀 Centralize a section criado no passo 6 (aquele que possui a classe right-content). Dica: para centralizar, basta configurar o margin-right: auto da section;
+section2.style.marginRight = 'auto';
+
 // 🚀 Troque a cor de fundo do elemento pai da section criada no passo 3 (aquela que possui a classe center-content) para a cor verde;
 // 🚀 Remova os dois últimos elementos (nove e dez) da lista criada no passo 8.
