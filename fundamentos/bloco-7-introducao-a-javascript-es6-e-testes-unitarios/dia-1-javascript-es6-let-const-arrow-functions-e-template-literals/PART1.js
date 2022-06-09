5d5d1038d69e6cd719024549ebfbb1eca7906a01
mjggel <luismiguelvieira015@gmail.com>
2022-06-09 15:21:59 -0300

    exercicios bloco 7 dia 1

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/PART1.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/PART1.js
@@ -16,15 +16,3 @@ function testingScope(escopo) {
   }
 
   testingScope(true);
-
-  //🚀2. Crie uma função que retorne um array em ordem crescente.
-  
-const oddsAndEvens = [13, 3, 4, 10, 7, 2];
-
-  // Seu código aqui.
-
-  console.log(oddsAndEvens); // será necessário alterar essa linha 😉
-
-// Crie uma função que faça com que o array oddsAndEvens fique na ordem crescente;
-// Utilize o console.log e template literals para retornar a frase: 'Os números 2,3,4,7,10,13 se encontram ordenados de forma crescente!'.
-// Utilize template literals para que a chamada console.log(<seu código>oddsAndEvens<seu código>); retorne a frase "Os números 2,3,4,7,10,13 se encontram ordenados de forma crescente!".
